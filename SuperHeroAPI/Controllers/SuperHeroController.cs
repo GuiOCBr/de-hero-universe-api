d5b8aa289634d6496a79a9b90bbba9f22bef1042
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2024-07-20 12:14:17 -0300

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuperHeroAPI/Entities/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI.Entities
+{
+    public class SuperHero
+    {
+        public Guid Id { get; set; }
+        public string HeroName { get; set; }
+        public string RealName { get; set;}
+        public string  MainPower { get; set; }
+
+    }
+}
